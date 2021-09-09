f8d6b5a04f68bdd6d14ca8e8fb8bef2118482421
Abid Shahriar <abidshahriar7@gmail.com>
2021-09-10 01:17:33 +0600

    done

--- script.js
+++ script.js
@@ -1,72 +1,82 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 var minionData = [
     {
         id: 1,
-        img: './images/minion/minion-01.jpg'
+        img: '/images/minion/minion-01.jpg'
     },
     {
         id: 2,
-        img: './images/minion/minion-02.jpg'
+        img: '/images/minion/minion-02.jpg'
     },
     {
         id: 3,
-        img: './images/minion/minion-03.jpg'
+        img: '/images/minion/minion-03.jpg'
     },
     {
         id: 4,
-        img: './images/minion/minion-04.jpg'
+        img: '/images/minion/minion-04.jpg'
     },
     {
         id: 5,
-        img: './images/minion/minion-05.jpg'
+        img: '/images/minion/minion-05.jpg'
     },
     {
         id: 6,
-        img: './images/minion/minion-06.jpg'
+        img: '/images/minion/minion-06.jpg'
     },
     {
         id: 7,
-        img: './images/minion/minion-07.jpg'
+        img: '/images/minion/minion-07.jpg'
     },
     {
         id: 8,
-        img: './images/minion/minion-08.jpg'
+        img: '/images/minion/minion-08.jpg'
     },
     {
         id: 9,
-        img: './images/minion/minion-09.jpg'
+        img: '/images/minion/minion-09.jpg'
     },
     {
         id: 10,
-        img: './images/minion/minion-10.jpg'
+        img: '/images/minion/minion-10.jpg'
     },
     {
         id: 11,
-        img: './images/minion/minion-11.jpg'
+        img: '/images/minion/minion-11.jpg'
     },
     {
         id: 12,
-        img: './images/minion/minion-12.jpg'
+        img: '/images/minion/minion-12.jpg'
     },
     {
         id: 13,
-        img: './images/minion/minion-13.jpg'
+        img: '/images/minion/minion-13.jpg'
     },
     {
         id: 14,
-        img: './images/minion/minion-14.jpg'
+        img: '/images/minion/minion-14.jpg'
     },
     {
         id: 15,
-        img: './images/minion/minion-15.jpg'
+        img: '/images/minion/minion-15.jpg'
     },
     {
         id: 16,
-        img: './images/minion/minion-16.jpg'
+        img: '/images/minion/minion-16.jpg'
     }
 ];
 var imagesContainer = document.getElementById('images-container');
-var startElement;
-var startElementSerial;
+var puzzleBox = document.querySelector('.puzzle-box');
+var shuffleBtn = document.getElementById('shuffle-btn');
+var message = document.querySelector('.message');
+var doneMessage = document.querySelector('.done-message');
+var playAgainBtn = document.getElementById('play-again-btn');
+var startElement = undefined;
+var startElementSerial = undefined;
 var allImageElements = [];
 generateLists(minionData);
 paintToDom(imagesContainer, allImageElements);
@@ -83,7 +93,7 @@ function generateLists(arr) {
         .forEach(function (item, index) {
         var imgContainer = document.createElement('div');
         imgContainer.setAttribute('data-index', "" + index);
-        imgContainer.innerHTML = "<img class='img' draggable=\"true\" src=\"" + item.img + "\" />";
+        imgContainer.innerHTML = "<img class='img' serial-index=\"" + item.id + "\" draggable=\"true\" src=\"" + item.img + "\" />";
         allImageElements.push(imgContainer);
     });
 }
@@ -95,7 +105,7 @@ function paintToDom(parentElement, arr) {
 }
 // add all drag events to all the images
 function addDragEvents(parentElement) {
-    var allChildren = Array.from(parentElement.children);
+    var allChildren = __spreadArray([], parentElement.children);
     allChildren.forEach(function (item) {
         item.children[0].addEventListener('dragstart', dragStart);
         item.children[0].addEventListener('dragenter', dragEnter);
@@ -107,7 +117,6 @@ function addDragEvents(parentElement) {
 function dragStart() {
     startElementSerial = +this.closest('div').getAttribute('data-index');
     startElement = allImageElements[startElementSerial].querySelector('.img');
-    console.log(startElement);
 }
 function dragEnter() {
     this.classList.add('over');
@@ -127,4 +136,38 @@ function dragDrop() {
     allContainers[startElementSerial].innerHTML = '';
     allContainers[serialIndex].appendChild(startElement);
     allContainers[startElementSerial].appendChild(dropElement);
+    puzzleDone();
 }
+function puzzleDone() {
+    var allContainers = document.querySelectorAll('#images-container > div > img');
+    var matchedElements = minionData.filter(function (item, idx) { return +allContainers[idx].getAttribute('serial-index') === item.id; });
+    if (matchedElements.length === minionData.length) {
+        shuffleBtn.classList.add('hidden');
+        message.classList.add('hidden');
+        doneMessage.classList.remove('hidden');
+        puzzleBox.classList.add('done');
+    }
+}
+function playAgain() {
+    shuffleBtn.classList.remove('hidden');
+    doneMessage.classList.add('hidden');
+    message.classList.remove('hidden');
+    puzzleBox.classList.remove('done');
+    startElement = undefined;
+    startElementSerial = undefined;
+    imagesContainer.innerHTML = '';
+    allImageElements = [];
+    generateLists(minionData);
+    paintToDom(imagesContainer, allImageElements);
+    addDragEvents(imagesContainer);
+}
+shuffleBtn.addEventListener('click', function () {
+    startElement = undefined;
+    startElementSerial = undefined;
+    imagesContainer.innerHTML = '';
+    allImageElements = [];
+    generateLists(minionData);
+    paintToDom(imagesContainer, allImageElements);
+    addDragEvents(imagesContainer);
+});
+playAgainBtn.addEventListener('click', playAgain);
--- script.tsx
+++ script.tsx
@@ -1,76 +1,81 @@
 const minionData = [
   {
     id: 1,
-    img: './images/minion/minion-01.jpg'
+    img: '/images/minion/minion-01.jpg'
   },
   {
     id: 2,
-    img: './images/minion/minion-02.jpg'
+    img: '/images/minion/minion-02.jpg'
   },
   {
     id: 3,
-    img: './images/minion/minion-03.jpg'
+    img: '/images/minion/minion-03.jpg'
   },
   {
     id: 4,
-    img: './images/minion/minion-04.jpg'
+    img: '/images/minion/minion-04.jpg'
   },
   {
     id: 5,
-    img: './images/minion/minion-05.jpg'
+    img: '/images/minion/minion-05.jpg'
   },
   {
     id: 6,
-    img: './images/minion/minion-06.jpg'
+    img: '/images/minion/minion-06.jpg'
   },
   {
     id: 7,
-    img: './images/minion/minion-07.jpg'
+    img: '/images/minion/minion-07.jpg'
   },
   {
     id: 8,
-    img: './images/minion/minion-08.jpg'
+    img: '/images/minion/minion-08.jpg'
   },
   {
     id: 9,
-    img: './images/minion/minion-09.jpg'
+    img: '/images/minion/minion-09.jpg'
   },
   {
     id: 10,
-    img: './images/minion/minion-10.jpg'
+    img: '/images/minion/minion-10.jpg'
   },
   {
     id: 11,
-    img: './images/minion/minion-11.jpg'
+    img: '/images/minion/minion-11.jpg'
   },
   {
     id: 12,
-    img: './images/minion/minion-12.jpg'
+    img: '/images/minion/minion-12.jpg'
   },
   {
     id: 13,
-    img: './images/minion/minion-13.jpg'
+    img: '/images/minion/minion-13.jpg'
   },
   {
     id: 14,
-    img: './images/minion/minion-14.jpg'
+    img: '/images/minion/minion-14.jpg'
   },
   {
     id: 15,
-    img: './images/minion/minion-15.jpg'
+    img: '/images/minion/minion-15.jpg'
   },
   {
     id: 16,
-    img: './images/minion/minion-16.jpg'
+    img: '/images/minion/minion-16.jpg'
   }
 ];
 
 const imagesContainer = document.getElementById('images-container')!;
+const puzzleBox = document.querySelector('.puzzle-box')!;
+const shuffleBtn = document.getElementById('shuffle-btn')!;
+const message = document.querySelector('.message')!;
+const doneMessage = document.querySelector('.done-message')!;
+const playAgainBtn = document.getElementById('play-again-btn')!;
 
-let startElement;
-let startElementSerial;
+let startElement = undefined;
+let startElementSerial = undefined;
 
-const allImageElements = [];
+let allImageElements = [];
 
 generateLists(minionData);
 
@@ -89,7 +94,7 @@ function generateLists(arr: { id: number; img: string }[]) {
 
       imgContainer.setAttribute('data-index', `${index}`);
 
-      imgContainer.innerHTML = `<img class='img' draggable="true" src="${item.img}" />`;
+      imgContainer.innerHTML = `<img class='img' serial-index="${item.id}" draggable="true" src="${item.img}" />`;
 
       allImageElements.push(imgContainer);
     });
@@ -103,9 +108,9 @@ function paintToDom(parentElement: HTMLElement, arr: string[]) {
 }
 
 // add all drag events to all the images
-function addDragEvents(parentElement: HTMLElement) {
-  const allChildren = Array.from(parentElement.children);
-  allChildren.forEach((item) => {
+function addDragEvents(parentElement: any) {
+  const allChildren = [...parentElement.children];
+  allChildren.forEach((item: any) => {
     item.children[0].addEventListener('dragstart', dragStart);
     item.children[0].addEventListener('dragenter', dragEnter);
     item.children[0].addEventListener('dragover', dragOver);
@@ -118,9 +123,8 @@ function dragStart() {
   startElementSerial = +this.closest('div').getAttribute('data-index');
 
   startElement = allImageElements[startElementSerial].querySelector('.img');
-
-  console.log(startElement);
 }
+
 function dragEnter() {
   this.classList.add('over');
 }
@@ -140,4 +144,52 @@ function dragDrop() {
 
   allContainers[serialIndex].appendChild(startElement);
   allContainers[startElementSerial].appendChild(dropElement);
+
+  puzzleDone();
 }
+
+function puzzleDone() {
+  const allContainers = document.querySelectorAll('#images-container > div > img')!;
+
+  const matchedElements = minionData.filter((item, idx) => +allContainers[idx].getAttribute('serial-index') === item.id);
+
+  if (matchedElements.length === minionData.length) {
+    shuffleBtn.classList.add('hidden');
+    message.classList.add('hidden');
+    doneMessage.classList.remove('hidden');
+    puzzleBox.classList.add('done');
+  }
+}
+
+function playAgain() {
+  shuffleBtn.classList.remove('hidden');
+  doneMessage.classList.add('hidden');
+  message.classList.remove('hidden');
+  puzzleBox.classList.remove('done');
+
+  startElement = undefined;
+  startElementSerial = undefined;
+  imagesContainer.innerHTML = '';
+  allImageElements = [];
+
+  generateLists(minionData);
+
+  paintToDom(imagesContainer, allImageElements);
+
+  addDragEvents(imagesContainer);
+}
+
+shuffleBtn.addEventListener('click', () => {
+  startElement = undefined;
+  startElementSerial = undefined;
+  imagesContainer.innerHTML = '';
+  allImageElements = [];
+
+  generateLists(minionData);
+
+  paintToDom(imagesContainer, allImageElements);
+
+  addDragEvents(imagesContainer);
+});
+
+playAgainBtn.addEventListener('click', playAgain);
